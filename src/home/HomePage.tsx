599a546617fba58c07a67e2c67bc5f9e1cdb6b29
Mike Cann <mike.cann@gmail.com>
2025-10-29 10:57:07 +0800

    wip

--- src/home/HomePage.tsx
+++ src/home/HomePage.tsx
@@ -9,8 +9,6 @@ import BookNowSection from "./BookNowSection";
 import { useQuery } from "convex/react";
 import { api } from "../../convex/_generated/api";
 import { HomeEntryMarker } from "./HomeEntryMarker";
-import { Activity } from "react";
-import * as LazyPages from "../common/lazy/LazyPages";
 
 export default function HomePage() {
   const randomEntries = useQuery(api.public.entries.getRandomThree);
