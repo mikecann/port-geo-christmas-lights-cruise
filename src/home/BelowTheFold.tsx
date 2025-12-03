1f431de9330a24fa70fc6d581b6f5f3a698857c7
Mike Cann <mike.cann@gmail.com>
2025-12-03 17:30:39 +0800

    updated copy

--- src/home/BelowTheFold.tsx
+++ src/home/BelowTheFold.tsx
@@ -6,55 +6,54 @@ export default function BelowTheFoldCopy() {
       <Container size="sm">
         <Box ta="center">
           <Title order={2} c="#FBAF5D" mb="sm">
-            Event details
+            Event Details
           </Title>
-          <Text c="gray.0" mb="md" size="lg" fw={600}>
-            The 2025 Port Geographe Christmas Lights Cruise is back and more
-            magical than ever!
+          <Text c="gray.0" mb="xs" size="md">
+            Departs Port Geographe Marina
           </Text>
           <Text c="gray.0" mb="xs" size="md">
-            Departs Port Geographe Marina | $18 per person
+            General Admission | $18 per ticket
           </Text>
-          <Text c="gray.0" mb="md" size="md">
-            Every Friday, Saturday and Sunday from 5th Dec | 7:30pm & 8:30pm
+          <Text c="gray.0" mb="xs" size="md">
+            Infant (under 12 months) | Free
           </Text>
-          <Text c="gray.0" mb="lg" size="md">
+          <Text c="gray.0" mb="xs" size="md">
+            Every Friday, Saturday and Sunday from 5th - 24th Dec | 7:30pm and
+            8:30pm
+          </Text>
+          <Text c="gray.0" mb="xs" size="md">
             All Sea Charters will take you through every canal and under the
             bridge, ensuring all displays are visible.
+          </Text>
+          <Text c="gray.0" mb="lg" size="md">
+            Queries: amcrostie@aigleroyal.com.au
           </Text>
 
           <Title order={2} c="#FBAF5D" mt="xl" mb="sm">
             Special Christmas Week Cruises
           </Title>
-          <Text c="gray.0" mb="xs" size="md">
+          <Text c="gray.0" mb="lg" size="md">
             Extra cruises will run Monday 22nd to Wednesday 24th December
             (Christmas Eve) - your final chance to experience the magic before
             Christmas!
           </Text>
-          <Text c="gray.0" mb="lg" size="md" fw={600}>
-            Please note: Not all cruise dates are released at once. To ensure
-            each cruise is full, new dates are made available once earlier
-            sessions reach capacity.
+
+          <Title order={2} c="#FBAF5D" mt="xl" mb="sm">
+            Minimum numbers:
+          </Title>
+          <Text c="gray.0" mb="lg" size="md">
+            To make this cruise happen, we need at least 50 tickets sold. If we
+            don't hit the mark, don't worry - you can either get a full refund
+            or choose another date to join the fun!
           </Text>
 
           <Title order={2} c="#FBAF5D" mt="xl" mb="sm">
-            Ticket redemption at Altair Estate Winery
+            Cruise schedule:
           </Title>
-
-          <Title order={3} c="#FBAF5D" mt="lg" mb="xs" size="h4">
-            Ticket holders - Wine Credit 2026
-          </Title>
-          <Text c="gray.0" mb="xs" size="md">
-            Each cruise ticket entitles you to a $18 wine credit at Altair
-            Estate, Wilyabrup. Purchase 4 tickets and receive $72 in wine
-            credit.
-          </Text>
-          <Text c="gray.0" mb="xs" size="md">
-            Simply visit our cellar door, enjoy a tasting and present your
-            cruise ticket – your credit will already be applied.
-          </Text>
-          <Text c="gray.0" mb="lg" size="md" fs="italic">
-            Offer valid: Wednesday, 7 January 2026 – Sunday, 29 February 2026.
+          <Text c="gray.0" mb="lg" size="md">
+            Not all cruise dates are released at once. To keep the decks full
+            and the fun buzzing, new dates are unlocked as earlier sessions sell
+            out
           </Text>
         </Box>
       </Container>
