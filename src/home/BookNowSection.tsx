7b8e11eeb9b8752ecf08bd16d394b9957b22f750
Mike Cann <mike.cann@gmail.com>
2025-12-22 13:29:25 +0800

    added vot button back

--- src/home/BookNowSection.tsx
+++ src/home/BookNowSection.tsx
@@ -1,10 +1,38 @@
-import { Box, Container } from "@mantine/core";
+import { Box, Button, Container, Stack } from "@mantine/core";
+import { useDisclosure } from "@mantine/hooks";
+import VoteModal from "./VoteModal";
 
 export default function BookNowSection() {
-  // Tickets sold out - hiding booking button
+  const [voteModalOpened, { open: openVoteModal, close: closeVoteModal }] =
+    useDisclosure(false);
+
   return (
-    <Box bg="#02051A" pb={20}>
-      <Container size="sm" />
-    </Box>
+    <>
+      <Box bg="#02051A" pb={20}>
+        <Container size="sm">
+          <Stack gap="sm">
+            <Button
+              size="xl"
+              radius="md"
+              variant="filled"
+              color="#C52630"
+              fullWidth
+              mt="md"
+              styles={{
+                root: {
+                  height: 80,
+                  fontSize: 24,
+                  fontWeight: 700,
+                },
+              }}
+              onClick={openVoteModal}
+            >
+              Vote Now
+            </Button>
+          </Stack>
+        </Container>
+      </Box>
+      <VoteModal opened={voteModalOpened} onClose={closeVoteModal} />
+    </>
   );
 }
