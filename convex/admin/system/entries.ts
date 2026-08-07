7086f771b36207785f07382669aebcf44663b4f8
Mike Cann <mike.cann@gmail.com>
2026-08-07 11:55:53 +0800

    Remove unsafe mock-data cleanup

--- convex/admin/system/entries.ts
+++ convex/admin/system/entries.ts
@@ -1,7 +1,6 @@
 import { v } from "convex/values";
 import { userSystemAdminMutation } from "./lib";
 import { entries } from "../../features/entries/model";
-import { api } from "../../_generated/api";
 import { createMockEntries } from "../../features/entries/testing";
 import { competitions } from "../../features/competitions/model";
 
@@ -30,44 +29,6 @@ export const wipeCurrentCompetition = userSystemAdminMutation
     return {
       message: `Successfully deleted ${result.deletedCount} current competition entries`,
       deletedCount: result.deletedCount,
-    };
-  })
-  .public();
-
-export const wipeAllTestUsers = userSystemAdminMutation
-  .input({})
-  .handler(async (context) => {
-    const allUsers = await context.db.query("users").collect();
-    const testUsers = allUsers.filter((user) => user.isTestUser === true);
-
-    let deletedCount = 0;
-    for (const user of testUsers) {
-      await context.db.delete(user._id);
-      deletedCount++;
-    }
-
-    return {
-      message: `Successfully deleted ${deletedCount} test users`,
-      deletedCount,
-    };
-  })
-  .public();
-
-export const wipeAllMockData = userSystemAdminMutation
-  .input({})
-  .handler(async (context) => {
-    await context.runMutation(
-      api.admin.system.entries.wipeCurrentCompetition,
-      {},
-    );
-    await context.runMutation(api.admin.system.entries.wipeAllTestUsers, {});
-    await context.runMutation(
-      api.admin.system.votes.wipeCurrentCompetition,
-      {},
-    );
-
-    return {
-      message: `Successfully deleted all entries and test users`,
     };
   })
   .public();
